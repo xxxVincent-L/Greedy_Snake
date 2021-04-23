50596c23c37322b7cf7c5e245ddedb2c8c5e2130
BaoHongHui <938326338@qq.com>
2021-04-23 17:38:55 +0800

    <feat>(<all>):   -Show the score   -Add bad food and bomb

--- Snake.py
+++ Snake.py
@@ -9,8 +9,13 @@ pygame.init()  # 初试化pygame
 white_colour = pygame.Color(255, 255, 255)  # 白色
 black_colour = pygame.Color(0, 0, 0)  # 黑色
 red_colour = pygame.Color(255, 0, 0)  # 红色
+green_colour = pygame.Color(0, 255, 0)  # 绿色
+yellow_colour = pygame.Color(150, 160, 0)  # 黄色
+blue_colour = pygame.Color(0, 0, 255)  # 蓝色
 game_surface = pygame.display.set_mode((600, 400))  # 设置pygame游戏框大小
 pygame.display.set_caption("贪吃蛇")  # 设置游戏标题
+# 初始化游戏界面内使用的字体
+font = pygame.font.SysFont("SIMYOU.TTF", 40)
 
 
 # 给自动运行的蛇改变方向
@@ -43,11 +48,26 @@ def new_head_position(head_position, direction):
         head_position[0] += 20
     return head_position
 
+
+# 得分显示
+def drawScore(score):
+    # 设置分数的显示颜色
+    Surf = font.render('Score:%s' % (score), True, green_colour)
+    # 设置分数的位置
+    Rect = Surf.get_rect()
+    Rect.midtop = (300, 200)
+    # 绑定以上设置到句柄
+    game_surface.blit(Surf, Rect)
+
+
 def main():
     head_position = [100, 100]  # 蛇的初始位置
     food_position = [200, 100]  # 食物的位置
+    bomb_position = []  # 炸弹的位置
+    bad_food_position = []  # 坏食物的位置
     snake_position = [[100, 100]]  # 蛇身的整个位置
     empty_position = []  # 空出的位置
+    score = 0  # 分数的初始化
     for i in range(30):
         for j in range(20):
             empty_position.append([i * 20, j * 20])
@@ -58,13 +78,24 @@ def main():
     EVENT_TIME = pygame.USEREVENT + 1  # 设置时延
     speed_time = 400
     pygame.time.set_timer(EVENT_TIME, speed_time)
+    # 炸弹与坏食物的位置设定
+    bomb_position = random.choice(empty_position)
+    empty_position.remove(bomb_position)
+    bad_food_position = random.choice(empty_position)
+    empty_position.remove(bad_food_position)
     while True:
+
         game_surface.fill(black_colour)  # 背景填充为黑色
         for i in range(len(snake_position)):  # 用循环来画整个蛇
             pygame.draw.rect(game_surface, white_colour,
                              Rect(snake_position[i][0], snake_position[i][1], 20, 20))  # 在[100, 100]处画20*20的矩形
         pygame.draw.rect(game_surface, red_colour,
                          Rect(food_position[0], food_position[1], 20, 20))  # 画出食物的位置
+        pygame.draw.rect(game_surface, yellow_colour,
+                         Rect(bomb_position[0], bomb_position[1], 20, 20))  # 画出炸弹的位置
+        pygame.draw.rect(game_surface, blue_colour,
+                         Rect(bad_food_position[0], bad_food_position[1], 20, 20))  # 画出不好的食物的位置
+        drawScore(score)  # 画出分数
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # 如果是退出键则退出游戏
@@ -83,10 +114,19 @@ def main():
 
                 if head_position in empty_position:  # 排除蛇头的位置
                     empty_position.remove(head_position)
-                if food_position == head_position:  # 蛇吃到食物，食物用随机数刷新
+
+                if food_position == head_position:  # 蛇吃到食物，食物用随机数刷新，分数+1
                     food_position = random.choice(empty_position)
+                    score += 1
+                elif bad_food_position == head_position:  # 蛇迟到坏食物，坏食物用随机数刷新，分数-1
+                    bad_food_position = random.choice(empty_position)
+                    score -= 1
+                elif bomb_position == head_position:  # 蛇碰到炸弹，直接结束
+                    sys.exit()
+
                 else:
                     empty_position.append(snake_position.pop())  # 每次移动，去除蛇尾，回归成空位置
+
             """
             # 添加按键移动功能w,a,s,d移动，坐标原点为左上角
             elif event.type == pygame.KEYDOWN:  # 获取按键的键值
@@ -99,7 +139,7 @@ def main():
             """
         if head_position[0] < 0 or head_position[0] > 600 \
                 or head_position[1] < 0 or head_position[1] > 400:
-            sys.exit()  # 到边界就退出
+            sys.exit()
 
         for i in snake_position[1:]:  # 当蛇头碰到蛇身时，直接退出游戏（利用一个切片
             if head_position[0] == i[0] and \
@@ -111,4 +151,3 @@ def main():
 
 main()
 
-# test
